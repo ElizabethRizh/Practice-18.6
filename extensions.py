5a4c7d098c712791294192e3016c96277af93930
ElizabethRizh <dddeliza@yandex.ru>
2023-07-19 18:51:14 +0300

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -2,34 +2,33 @@ import requests
 import json
 from config import keys
 
-class APIException(Exception):
+
+class ConvertionException(Exception):
     pass
+
 
 class CryptoConverter:
     @staticmethod
-    def get_price(base: str, quote: str, amount: str):
+    def get_price(quote: str, base: str, amount: str):
         if quote == base:
-            raise APIException(f'Невозможно перевести одинаковые валюты {base}.')
+            raise ConvertionException(f'Невозможно перевести одинаковые валюты в {base}.')
 
         try:
             quote_ticker = keys[quote]
         except KeyError:
-            raise APIException(f'Не удалось обработать валюту {quote}')
+            raise ConvertionException(f'Не удалось обработать валюту {quote}.')
 
         try:
             base_ticker = keys[base]
         except KeyError:
-            raise APIException(f'Не удалось обработать валюту {base}')
+            raise ConvertionException(f'Не удалось обработать валюту {base}.')
 
         try:
             amount = float(amount)
         except ValueError:
-            raise APIException(f'Не удалось обработать количество валюты{amount}')
+            raise ConvertionException(f'Не удалось обработать количество {amount}.')
 
-        if amount <= 0:
-            raise APIException(f'Невозможно конверстировать количество валюты меньше или равное 0')
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
 
-        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
-        total_base = json.loads(r.content)[keys[quote]]
-
-        return total_base
+        return total_base * amount
